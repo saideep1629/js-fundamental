f3dabb9fcc6695e535e750d9d8d6cb0a36aa6893
saideep1629 <saideepmanve3@gmail.com>
2025-07-30 23:06:20 +0530

    day 7 done

--- arrayMethodsImp.js
+++ arrayMethodsImp.js
@@ -1,28 +1,31 @@
 
-// // const arr = [1, 2, 3, 4, 5, 6, 7, 8];
-// // const result = arr.reduce(
-// //     function(acc, curVal){
-// //         console.log(`acc: ${acc} and currVal ${curVal}`);
-// //         return acc + curVal;
-// //     },0);
-// // console.log(result);
+// // // const arr = [1, 2, 3, 4, 5, 6, 7, 8];
+// // // const result = arr.reduce(
+// // //     function(acc, curVal){
+// // //         console.log(`acc: ${acc} and currVal ${curVal}`);
+// // //         return acc + curVal;
+// // //     },0);
+// // // console.log(result);
 
 
-// //  Group items
+// // //  Group items
 
-// // const fruits = ['apple', 'banana', 'apple', 'orange', 'banana'];
-// // const count = fruits.reduce((acc, fruit) => {
-// //   acc[fruit] = (acc[fruit] || 0) + 1;
-// //   console.log(acc);
-// //   return acc;
-// // }, {});
-// // console.log(count); 
+const fruits = ['apple', 'banana', 'apple', 'orange', 'banana'];
+const count = fruits.reduce((acc, fruit) => {
+  acc[fruit] = (acc[fruit] || 0) + 1;
+  return acc;
+}, {});
+console.log(count);
+// { apple: 2, banana: 2, orange: 1 } 
 
-// // reduce()
-// // reduce array to single value
-// // return single value
-// // does not mutate original array
-// // aggregate data
+
+
+
+// reduce()
+// reduce array to single value
+// return single value
+// does not mutate original array
+// aggregate data
 
 // // ********************************************************
 
@@ -56,14 +59,19 @@
 
 
 
-const arr = [ 1, 2, 3, 4, 5, 10, 20, 30, 40, 50];
+const arr = [ 10, 20, 30, 40 ];
 
-// i want a newArray with elements greater than 5
+const add = arr.reduce((acc, currVal) => {
+  return acc + currVal;
+  
+})
 
-const newArr = arr.filter((val) => (val > 5));
+console.log(add); // 100
 
-console.log(newArr);  //  [10, 20, 30, 40, 50]
-console.log(arr); //  [ 1, 2, 3, 4, 5, 10, 20, 30, 40, 50]
+
+
+
+
 
 
 
